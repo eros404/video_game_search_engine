68993c32ccc527ae708b49b6a7e2881794d883a8
Louis Morando <louis.morando@epita.fr>
2022-01-26 17:28:36 +0100

    AMQP to ElasticSearch

---
+++ search-api/src/main/java/fr/lernejo/search/api/GameInfoListener.java
@@ -0,0 +1,36 @@
+package fr.lernejo.search.api;
+
+import com.rabbitmq.client.Channel;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GameInfoListener {
+    private final Logger logger = LoggerFactory.getLogger(GameInfoListener.class);
+    private final RestHighLevelClient client;
+    public  GameInfoListener(RestHighLevelClient client) {
+        this.client = client;
+    }
+    @RabbitListener(queues = AmqpConfiguration.GAME_INFO_QUEUE)
+    public void onMessage(String message, @Header("game_id") String gameId) {
+        IndexRequest request = new IndexRequest("games")
+            .id(gameId)
+            .source(message, XContentType.JSON);
+        try {
+            client.index(request, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
