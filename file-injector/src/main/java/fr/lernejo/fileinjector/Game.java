b3ebdc8001ead8d3b9d4d8b5efaff2152b2d685c
Eros404 <com.louismorando@gmail.com>
2022-01-27 16:26:37 +0100

    Send messages from JSON to AMQP

---
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Game.java
@@ -0,0 +1,15 @@
+package fr.lernejo.fileinjector;
+
+public class Game {
+    public Integer id;
+    public String title;
+    public String thumbnail;
+    public String short_description;
+    public String game_url;
+    public String genre;
+    public String platform;
+    public String publisher;
+    public String developer;
+    public String release_date;
+    public String freetogame_profile_url;
+}
--- file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
+++ file-injector/src/main/java/fr/lernejo/fileinjector/Launcher.java
@@ -1,16 +1,30 @@
 package fr.lernejo.fileinjector;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class Launcher {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("You must provide 1 parameter.");
+            throw new Error();
+        }
         try (AbstractApplicationContext springContext = new AnnotationConfigApplicationContext(Launcher.class)) {
-
-            System.out.println("Hello after starting Spring");
+            Game[] games = new ObjectMapper().readValue(Paths.get(args[0]).toFile(), Game[].class);
+            RabbitTemplate rabbitTemplate = springContext.getBean(RabbitTemplate.class);
+            rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+            for (Game game : games) {
+                rabbitTemplate.convertAndSend("", "game_info", game);
+            }
         }
     }
 }
