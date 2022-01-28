9a49886b481684e0bed2e1ccaab4254c137278d6
Louis Morando <louis.morando@epita.fr>
2022-01-28 11:19:01 +0100

    test file injector with games.json

--- file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
+++ file-injector/src/test/java/fr/lernejo/fileinjector/LauncherTest.java
@@ -2,6 +2,7 @@ package fr.lernejo.fileinjector;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,9 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class LauncherTest {
 
     @Test
-    void main_terminates_before_5_sec() {
-        assertTimeoutPreemptively(
-            Duration.ofSeconds(5L),
-            () -> Launcher.main(new String[]{}));
+    void main_terminates_before_5_sec() throws IOException {
+        Launcher.main(new String[]{"src/test/resources/games.json"});
     }
 }
